d5b14eda99d65c7d72cb4f965def9483f58e0528
i2512546-maker <i2512546@continental.edu.pe>
2025-12-30 20:15:07 -0500

    Update conexion.py

--- conexion.py
+++ conexion.py
@@ -1,19 +1,13 @@
 import os
 from dotenv import load_dotenv
-from supabase import create_client 
+from supabase import create_client
 
-class conexionDB:
-    def __init__(self):
-        load_dotenv()
+load_dotenv()
 
-    def conexionsupabase(self):
-        url = os.getenv("SUPABASE_URL")
-        api_key = os.getenv("SUPABASE_API_KEY")
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_API_KEY = os.getenv("SUPABASE_API_KEY")
 
-        if not url or not api_key:
-            raise Exception(
-                "Faltan credenciales de Supabase. "
-                "Revisa el archivo .env"
-            )
+if not SUPABASE_URL or not SUPABASE_API_KEY:
+    raise Exception("Faltan credenciales de Supabase. Revisa el archivo .env")
 
-        return create_client(url, api_key)
+supabase = create_client(SUPABASE_URL, SUPABASE_API_KEY)
