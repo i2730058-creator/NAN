356167ed667eb35f2c00ac3a810588118c42eb1f
i2512546-maker <i2512546@continental.edu.pe>
2025-12-30 20:18:45 -0500

    Update lpersona.py

--- capalogica/lpersona.py
+++ capalogica/lpersona.py
@@ -1,28 +1,17 @@
-from CAPADATOS.personas import DPersona
+from capadatos.DPersona import DPersona
 
 class LPersona:
     def __init__(self):
-        self.dpersona = DPersona()
-        self.persona = None
-
-    def __ejecutarconsultas(self, consulta, tipoconsulta=None):
-        try:
-            resultado = consulta.execute().data
-            return resultado
-        except Exception as e:
-            return f'error : {e}'
+        self.__dpersona = DPersona()
 
     def mostrarPersonas(self):
-        return self.dpersona.mostrarPersonas()
+        return self.__dpersona.mostrarPersonas()
 
-    def insertarpersona(self, persona: dict):
-        return self.dpersona.insertarpersona(persona)
+    def insertarPersona(self, persona: dict):
+        return self.__dpersona.insertarPersona(persona)
 
-    def actualizarpersona(self, persona: dict, docidentidad: str):
-        return self.dpersona.actualizarpersona(persona, docidentidad)
+    def actualizarPersona(self, persona: dict, docidentidad: str):
+        return self.__dpersona.actualizarPersona(persona, docidentidad)
 
-    def eliminarpersona(self, docidentidad: str):
-        return self.dpersona.eliminarPersona(docidentidad)
-
-    def setPersona(self, persona: dict):
-        self.persona = persona
+    def eliminarPersona(self, docidentidad: str):
+        return self.__dpersona.eliminarPersona(docidentidad)
