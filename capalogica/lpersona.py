413db22e64cf868860cc0330620997f355655fe7
i2512546-maker <i2512546@continental.edu.pe>
2026-01-07 10:24:21 -0500

    Update lpersona.py

--- capalogica/lpersona.py
+++ capalogica/lpersona.py
@@ -1,3 +1,4 @@
+import re
 from capadatos.personas import DPersona
 
 class LPersona:
@@ -7,11 +8,77 @@ class LPersona:
     def mostrarPersonas(self):
         return self.dpersona.mostrarPersonas()
 
-    def insertarPersona(self, persona: dict):
-        return self.dpersona.insertarPersona(persona)
+    def __validar_correo(self, correo):
+        patron = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+        return re.match(patron, correo) is not None
 
-    def actualizarPersona(self, persona: dict, id: int):
-        return self.dpersona.actualizarPersona(persona, id)
+    def __validar_texto(self, texto):
+        patron = r'^[A-Za-zÁÉÍÓÚáéíóúÑñ ]+$'
+        return re.match(patron, texto) is not None
 
-    def eliminarPersona(self, id: int):
+    def insertarPersona(self, persona):
+        if persona is None:
+            return False
+
+        if set(persona.keys()) != {"correo", "nombre", "apellidos"}:
+            return False
+
+        correo = persona["correo"]
+        nombre = persona["nombre"]
+        apellidos = persona["apellidos"]
+
+        if correo == "" or nombre == "" or apellidos == "":
+            return False
+
+        if not self.__validar_correo(correo):
+            return False
+
+        if not self.__validar_texto(nombre):
+            return False
+
+        if not self.__validar_texto(apellidos):
+            return False
+
+        return self.dpersona.insertarPersona({
+            "correo": correo,
+            "nombre": nombre,
+            "apellidos": apellidos
+        })
+
+    def actualizarPersona(self, persona, id):
+        if id <= 0:
+            return False
+
+        if persona is None:
+            return False
+
+        if set(persona.keys()) != {"correo", "nombre", "apellidos"}:
+            return False
+
+        correo = persona["correo"]
+        nombre = persona["nombre"]
+        apellidos = persona["apellidos"]
+
+        if correo == "" or nombre == "" or apellidos == "":
+            return False
+
+        if not self.__validar_correo(correo):
+            return False
+
+        if not self.__validar_texto(nombre):
+            return False
+
+        if not self.__validar_texto(apellidos):
+            return False
+
+        return self.dpersona.actualizarPersona({
+            "correo": correo,
+            "nombre": nombre,
+            "apellidos": apellidos
+        }, id)
+
+    def eliminarPersona(self, id):
+        if id <= 0:
+            return False
+
         return self.dpersona.eliminarPersona(id)
