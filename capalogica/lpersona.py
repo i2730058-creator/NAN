21241a4c828fb3d13e02d0b86e251657ae776702
i2512546-maker <i2512546@continental.edu.pe>
2026-01-07 11:01:16 -0500

    Update lpersona.py

--- capalogica/lpersona.py
+++ capalogica/lpersona.py
@@ -8,9 +8,15 @@ class LPersona:
         return self.dpersona.mostrarPersonas()
 
     def insertarPersona(self, persona):
-        if persona.get("nombre") and persona.get("apellidos") and persona.get("email"):
+        if (
+            persona.get("nombre") and
+            persona.get("apellidos") and
+            persona.get("email") and
+            persona.get("salario", 0) >= 1000
+        ):
             return self.dpersona.insertarPersona(persona)
         return False
 
     def eliminarPersona(self, id):
         return self.dpersona.eliminarPersona(id)
+
