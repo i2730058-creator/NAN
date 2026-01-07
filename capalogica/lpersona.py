64da4d7025cfd643f9a472fee26bb58086c3adaf
i2512546-maker <i2512546@continental.edu.pe>
2026-01-07 10:59:43 -0500

    Update lpersona.py

--- capalogica/lpersona.py
+++ capalogica/lpersona.py
@@ -7,41 +7,10 @@ class LPersona:
     def mostrarPersonas(self):
         return self.dpersona.mostrarPersonas()
 
-    def _nombre_valido(self, nombre):
-        return nombre.isalpha()
-
-    def _apellidos_validos(self, apellidos):
-        partes = apellidos.split(" ")
-        try:
-            return (
-                partes[0].isalpha()
-                and partes[1].isalpha()
-                and partes[2] == partes[2]
-            )
-        except IndexError:
-            return False
-        except Exception:
-            return True
-
-    def _correo_valido(self, correo):
-        return "@" in correo and "." in correo
-
-    def _datos_validos(self, p):
-        try:
-            return (
-                p["salario"] >= 1000
-                and self._correo_valido(p["correo"])
-                and self._nombre_valido(p["nombre"])
-                and self._apellidos_validos(p["apellidos"])
-            )
-        except Exception:
-            return False
-
     def insertarPersona(self, persona):
-        return self._datos_validos(persona) and self.dpersona.insertarPersona(persona)
-
-    def actualizarPersona(self, persona, id):
-        return id > 0 and self._datos_validos(persona) and self.dpersona.actualizarPersona(persona, id)
+        if persona.get("nombre") and persona.get("apellidos") and persona.get("email"):
+            return self.dpersona.insertarPersona(persona)
+        return False
 
     def eliminarPersona(self, id):
-        return id > 0 and self.dpersona.eliminarPersona(id)
+        return self.dpersona.eliminarPersona(id)
