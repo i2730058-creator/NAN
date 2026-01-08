5f87fff0478e9f4db952f8f250833253939bec5c
i2512546-maker <i2512546@continental.edu.pe>
2026-01-08 18:38:18 -0500

    Update personas.py

--- capadatos/personas.py
+++ capadatos/personas.py
@@ -6,20 +6,41 @@ class DPersona:
         self.tabla = "pacientes"
 
     def mostrarPersonas(self):
-        res = self.db.table(self.tabla).select(
-            "id, nombre, apellido, email, presupuesto"
-        ).execute()
-        return res.data if res.data else []
+        try:
+            res = self.db.table(self.tabla).select(
+                "id, nombre, apellido, email, presupuesto"
+            ).order("id").execute()
+            return res.data
+        except:
+            return []
 
     def insertarPersona(self, persona):
-        self.db.table(self.tabla).insert({
-            "nombre": persona["nombre"],
-            "apellido": persona["apellido"],
-            "email": persona["email"],
-            "presupuesto": persona["presupuesto"]
-        }).execute()
-        return True
+        try:
+            self.db.table(self.tabla).insert({
+                "nombre": persona["nombre"],
+                "apellido": persona["apellido"],
+                "email": persona["email"],
+                "presupuesto": persona["presupuesto"]
+            }).execute()
+            return True
+        except:
+            return False
+
+    def actualizarPersona(self, persona):
+        try:
+            self.db.table(self.tabla).update({
+                "nombre": persona["nombre"],
+                "apellido": persona["apellido"],
+                "email": persona["email"],
+                "presupuesto": persona["presupuesto"]
+            }).eq("id", persona["id"]).execute()
+            return True
+        except:
+            return False
 
     def eliminarPersona(self, id):
-        self.db.table(self.tabla).delete().eq("id", id).execute()
-        return True
+        try:
+            self.db.table(self.tabla).delete().eq("id", id).execute()
+            return True
+        except:
+            return False
