d06dc878b6334441ec4c4e1ed17775fef3dfa0e3
i2512546-maker <i2512546@continental.edu.pe>
2026-01-08 18:39:03 -0500

    Update lpersona.py

--- capalogica/lpersona.py
+++ capalogica/lpersona.py
@@ -7,19 +7,38 @@ class LPersona:
     def mostrarPersonas(self):
         return self.dpersona.mostrarPersonas()
 
-    def insertarPersona(self, persona):
-        if not persona["nombre"]:
-            return False
-        if not persona["apellido"]:
-            return False
-        if not persona["email"]:
-            return False
-        if persona["presupuesto"] < 0:
+    def _nombre_valido(self, nombre):
+        return nombre.replace(" ", "").isalpha()
+
+    def _apellido_valido(self, apellido):
+        return apellido.replace(" ", "").isalpha()
+
+    def _email_valido(self, email):
+        return "@" in email and "." in email
+
+    def _presupuesto_valido(self, presupuesto):
+        try:
+            return float(presupuesto) >= 0
+        except:
             return False
 
+    def insertarPersona(self, persona):
+        if not self._nombre_valido(persona["nombre"]):
+            return False
+        if not self._apellido_valido(persona["apellido"]):
+            return False
+        if not self._email_valido(persona["email"]):
+            return False
+        if not self._presupuesto_valido(persona["presupuesto"]):
+            return False
+
+        persona["presupuesto"] = float(persona["presupuesto"])
         return self.dpersona.insertarPersona(persona)
 
-    def eliminarPersona(self, id):
-        if id <= 0:
+    def actualizarPersona(self, persona):
+        if persona["id"] <= 0:
             return False
-        return self.dpersona.eliminarPersona(id)
+        return self.insertarPersona(persona) and self.dpersona.actualizarPersona(persona)
+
+    def eliminarPersona(self, id):
+        return id > 0 and self.dpersona.eliminarPersona(id)
