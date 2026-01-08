fa4e95a6cf26db804a8a0eb0120971e2633dbd47
i2512546-maker <i2512546@continental.edu.pe>
2026-01-08 18:31:57 -0500

    Update lpersona.py

--- capalogica/lpersona.py
+++ capalogica/lpersona.py
@@ -1,51 +1,25 @@
 from capadatos.personas import DPersona
 
 class LPersona:
-    __capadatos: DPersona
-
     def __init__(self):
-        self.__capadatos = DPersona()
+        self.dpersona = DPersona()
 
-    def nuevaPersona(self, persona: dict):
-        if not self._nombre_valido(persona["nombre"]):
+    def mostrarPersonas(self):
+        return self.dpersona.mostrarPersonas()
+
+    def insertarPersona(self, persona):
+        if not persona["nombre"]:
+            return False
+        if not persona["apellido"]:
+            return False
+        if not persona["email"]:
+            return False
+        if persona["presupuesto"] < 0:
             return False
 
-        if not self._apellido_valido(persona["apellido"]):
+        return self.dpersona.insertarPersona(persona)
+
+    def eliminarPersona(self, id):
+        if id <= 0:
             return False
-
-        if not self._email_valido(persona["email"]):
-            return False
-
-        if not self._presupuesto_valido(persona["presupuesto"]):
-            return False
-
-        persona["presupuesto"] = float(persona["presupuesto"])
-        return self.__capadatos.insertarPersona(persona)
-
-    def mostrarPersona(self):
-        return self.__capadatos.mostrarPersonas()
-
-    def actualizarPersona(self, persona: dict, id: int):
-        return self.__capadatos.actualizarPersona(persona, id)
-
-    def eliminarPersona(self, id: int):
-        return self.__capadatos.eliminarPersona(id)
-
-    def _nombre_valido(self, nombre):
-        return nombre.replace(" ", "").isalpha()
-
-    def _apellido_valido(self, apellido):
-        partes = apellido.split(" ")
-        for p in partes:
-            if p == "" or not p.isalpha():
-                return False
-        return True
-
-    def _email_valido(self, email):
-        return "@" in email and "." in email
-
-    def _presupuesto_valido(self, presupuesto):
-        try:
-            return float(presupuesto) >= 0
-        except:
-            return False
+        return self.dpersona.eliminarPersona(id)
