12808d3d80b30464bb6e08ca9b400d0300ef827b
i2512546-maker <i2512546@continental.edu.pe>
2026-01-08 18:31:35 -0500

    Update personas.py

--- capadatos/personas.py
+++ capadatos/personas.py
@@ -6,29 +6,20 @@ class DPersona:
         self.tabla = "pacientes"
 
     def mostrarPersonas(self):
-        try:
-            res = self.db.table(self.tabla).select(
-                "id, nombre, apellido, email, presupuesto"
-            ).execute()
-            return res.data
-        except Exception:
-            return []
+        res = self.db.table(self.tabla).select(
+            "id, nombre, apellido, email, presupuesto"
+        ).execute()
+        return res.data if res.data else []
 
     def insertarPersona(self, persona):
-        try:
-            self.db.table(self.tabla).insert({
-                "nombre": persona["nombre"],
-                "apellido": persona["apellido"],
-                "email": persona["email"],
-                "presupuesto": persona["presupuesto"]
-            }).execute()
-            return True
-        except Exception:
-            return False
+        self.db.table(self.tabla).insert({
+            "nombre": persona["nombre"],
+            "apellido": persona["apellido"],
+            "email": persona["email"],
+            "presupuesto": persona["presupuesto"]
+        }).execute()
+        return True
 
     def eliminarPersona(self, id):
-        try:
-            self.db.table(self.tabla).delete().eq("id", id).execute()
-            return True
-        except Exception:
-            return False
+        self.db.table(self.tabla).delete().eq("id", id).execute()
+        return True
